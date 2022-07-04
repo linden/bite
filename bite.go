6e6bb999fee4427ca45b2559039e3eb5deb9a618
Linden <70739041+linden@users.noreply.github.com>
2022-07-04 16:17:24 -0700

    feat: add `Writer`

--- bite.go
+++ bite.go
@@ -1,8 +1,19 @@
 package bite
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 type Reader struct {
 	Value []byte
 }
+
+type Writer struct {
+	Value []byte
+}
+
+var BinaryEncoding = binary.BigEndian
 
 func (reader *Reader) Read(length int) []byte {
 	cursor := reader.Value[:length]
@@ -15,6 +26,62 @@ func (reader *Reader) ReadSingle() byte {
 	return reader.Read(1)[0]
 }
 
-func New(raw []byte) Reader {
+func NewReader(raw []byte) Reader {
 	return Reader{raw}
+}
+
+func (writer *Writer) Write(content any) {
+	switch any(content).(type) {
+	case []byte:
+		writer.Value = append(writer.Value, any(content).([]byte)...)
+
+	case byte:
+		writer.Value = append(writer.Value, any(content).(byte))
+
+	case string:
+		writer.Value = append(writer.Value, []byte(any(content).(string))...)
+
+	case int:
+		buffer := new(bytes.Buffer)
+
+		binary.Write(buffer, BinaryEncoding, int8(any(content).(int)))
+
+		writer.Value = append(writer.Value, buffer.Bytes()...)
+
+	default:
+		panic("type cannot be written")
+	}
+}
+
+func (writer *Writer) WriteWithLength(content any, length int) {
+	before := len(writer.Value)
+	padding := true
+
+	switch any(content).(type) {
+	case []byte, byte, string:
+		writer.Write(content)
+
+	case int:
+		padding = false
+
+		buffer := new(bytes.Buffer)
+
+		binary.Write(buffer, BinaryEncoding, int8(any(content).(int)))
+
+		for len(writer.Value)+buffer.Len() < (before + length) {
+			writer.Value = append(writer.Value, 0)
+		}
+
+		writer.Value = append(writer.Value, buffer.Bytes()...)
+	}
+
+	if padding == true {
+		for len(writer.Value) < (before + length) {
+			writer.Value = append(writer.Value, 0)
+		}
+	}
+}
+
+func NewWriter() Writer {
+	return Writer{}
 }
